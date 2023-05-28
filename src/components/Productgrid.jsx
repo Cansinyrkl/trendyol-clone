ee224844b2a094464e3b4fd747154eb335c50616
acyurekli <acyurekli@yukatech.com>
2023-05-28 18:55:07 +0300

    feat: product gird added

--- src/components/Productgrid.jsx
+++ src/components/Productgrid.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Grid, Image } from "theme-ui";
-import grid1 from "../image/grid1.jpg";
-import grid2 from "../image/grid2.jpg";
-import grid3 from "../image/grid3.jpg";
+import bottomgrid1 from "../image/bottomgrid1.jpg";
+import bottomgrid2 from "../image/bottomgrid2.jpg";
+import bottomgrid3 from "../image/bottomgrid3.jpg";
 
-const Undergridsupport = () => {
+const Productgrid = () => {
   return (
     <>
-      <Grid width={[128, 192]} F>
+      <Grid width={[128, 192]}>
         <Image
-          src={grid2}
+          src={bottomgrid1}
           style={{
             margin: "0px 40px 10px 0px",
             cursor: "pointer",
@@ -17,7 +17,7 @@ const Undergridsupport = () => {
           }}
         />
         <Image
-          src={grid1}
+          src={bottomgrid2}
           style={{
             margin: "0px 40px 10px 0px",
             cursor: "pointer",
@@ -25,7 +25,7 @@ const Undergridsupport = () => {
           }}
         />
         <Image
-          src={grid3}
+          src={bottomgrid3}
           style={{
             margin: "0px 40px 10px 0px",
             cursor: "pointer",
@@ -37,4 +37,4 @@ const Undergridsupport = () => {
   );
 };
 
-export default Undergridsupport;
+export default Productgrid;
--- src/page/home.jsx
+++ src/page/home.jsx
@@ -3,6 +3,9 @@ import Topbar from "../components/Topbar";
 import { Box } from "theme-ui";
 import Undertoptitle from "../components/Undertoptitle";
 import Undergridsupport from "../components/Undergridsupport";
+import Productgrid from "../components/Productgrid";
+import Brandtitle from "../components/Brandtitle";
+import Bestsellingproducts from "../components/Bestsellingproducts";
 
 const Home = () => {
   return (
@@ -10,6 +13,9 @@ const Home = () => {
       <Topbar />
       <Undertoptitle />
       <Undergridsupport />
+      <Productgrid />
+      <Brandtitle />
+      <Bestsellingproducts />
     </Box>
   );
 };
